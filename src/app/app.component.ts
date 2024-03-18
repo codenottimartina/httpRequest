8f7a3c7a7a92eb206431430de9321738ad02d007
Martina <martina.codenotti2@gmail.com>
2024-03-18 12:30:13 +0100

    fino a lezione 262

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http';
+
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http
+    .post<{name: string}>(
+      'https://progettocorsoangular-default-rtdb.firebaseio.com/posts.json', // URL
+      postData // body
+      ).subscribe(responseData => {
+        console.log(responseData);
+      }); 
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get<{ [key: string]: Post }>('https://progettocorsoangular-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postsArray: Post[] = [];
+      for(const key in responseData){
+        if(responseData.hasOwnProperty(key)){
+          postsArray.push(responseData[key]);
+        }
+      }
+      return postsArray;
+    }))
+    .subscribe(posts => {
+      console.log(posts);
+      
+    });
+  }
 }
